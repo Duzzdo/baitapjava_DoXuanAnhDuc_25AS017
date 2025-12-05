e5f43b18abbfa4cafd0d427de0abcfc70def569d
Duzzdo <anhduck2007@gmail.com>
2025-12-05 09:03:06 +0700

    Delete src directory

--- src/bai6_5/GeometriObject.java
+++
@@ -1,6 +0,0 @@
-package bai6_5;
-
-public interface GeometriObject {
-    public double getArea();
-    public double getPerimeter();
-}
--- src/bai6_5/Resizable.java
+++
@@ -1,5 +0,0 @@
-package bai6_5;
-
-public interface Resizable {
-    public void resize(int percent);
-}
