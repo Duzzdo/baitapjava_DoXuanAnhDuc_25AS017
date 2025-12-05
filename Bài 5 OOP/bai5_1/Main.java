314eb0ee51826e6fe841ddce464cd9dd4bd5805b
Duzzdo <anhduck2007@gmail.com>
2025-12-05 09:00:35 +0700

    Bài 5 OOP

---
+++ Bài 5 OOP/bai5_1/Main.java
@@ -0,0 +1,22 @@
+package bai5_1;
+
+public class Main {
+    static void main(String[] args) {
+        Point p1 = new Point(2,4);
+        System.out.println(p1);
+        p1.setX(5);
+        System.out.println(p1);
+        p1.setXY(6,5);
+        System.out.println(p1);
+        Line l1= new Line(4,6,8,10);
+        System.out.println(l1);
+        Point p2= new Point(6,10);
+        Line l2= new Line(p1,p2);
+        System.out.println(l2);
+        System.out.println(l2.getLength());
+        System.out.println(l1.getLength());
+        System.out.println(l1.getGradient());
+        System.out.println(l2.getGradient());
+    }
+}
+
